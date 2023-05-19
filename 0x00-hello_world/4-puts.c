70e17c7f7b8bd62a33670235a59a41a4e7a7aea1
MXCALL <idbellam12@gmail.com>
2023-05-18 17:17:17 -0700

    TASK4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description : "4.Hello, puts"
+ *
+ * @void : The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
