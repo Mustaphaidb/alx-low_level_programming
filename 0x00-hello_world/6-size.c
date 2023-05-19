24e2956ed9a573cca83e37db7dcdff567e7e4360
MXCALL <idbellam12@gmail.com>
2023-05-18 18:10:27 -0700

    TASK6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description : "6. Size of various types"
+ *
+ * @void : The main function takes no parameters
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
-printf("Size of a char: %zu byte(s)\n",sizeof(char));
-printf("Size of an int: %zu byte(s)\n",sizeof(int));
-printf("Size of a long int: %zu byte(s)\n",sizeof(long int));
-printf("Size of a long long int: %zu byte(s)\n",sizeof(long long int));
-printf("Size of a float: %zu byte(s)\n",sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 
 return (0);
