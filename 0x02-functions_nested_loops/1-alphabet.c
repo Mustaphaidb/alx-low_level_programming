4835f26ac6eecd34a6f6c6ca2c8246d345a1bf16
MXCALL <idbellam12@gmail.Com>
2023-05-24 23:55:45 +0100

    TASK1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
-/***/
+/**
+ * print_alphabet - entry point
+ *
+ * Description - 'Task1 print alphabet lowercase'
+ * @void : the function takes no parameteres
+*/
 
 void print_alphabet(void)
 {
 
 int a;
 
-    for (a = 97; a <= 122 ;a++)
-    {
-        _putchar(a);
-    }
-        _putchar('\n');
+for (a = 97; a <= 122 ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
 }
