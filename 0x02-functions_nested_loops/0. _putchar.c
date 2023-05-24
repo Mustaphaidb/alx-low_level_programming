f8cf679c6786f4ae803124d7c9e1613acece9718
MXCALL <idbellam12@gmail.Com>
2023-05-24 20:36:37 +0100

    README.md

---
+++ 0x02-functions_nested_loops/0. _putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - This is an Entry point
+ * Description : "TASK1 The last digit"
+ * @void : The function takes no parameters
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+
+
+	return (0);
+}
