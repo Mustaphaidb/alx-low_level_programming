65f668ee73ebbcab3d6ac4010508fa9469eb872b
MXCALL <idbellam12@gmail.Com>
2023-05-24 18:19:03 +0100

    TASK2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
-int main (void){
+/**
+ * main - this is an Entry point
+ *
+ * @void - tasks no parametres
+ *
+ * Description - 'TASK2 print alphabet'
+ * Return: 0 (always)
+ *
+*/
+
+int main(void)
+{
 
 char A;
-    for (A = 'a'; A <= 'z'; A++)
-        putchar(A);
-    
-    putchar('\n');
+for (A = 'a'; A <= 'z'; A++)
+putchar(A);
+putchar('\n');
 
-
-    return (0);
+return (0);
 }
