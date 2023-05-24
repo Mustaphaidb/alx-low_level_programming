9ee00799a5f4d594445337238b8ea50473b52307
MXCALL <idbellam12@gmail.Com>
2023-05-25 00:36:23 +0100

    TASK2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- * print_alphabet - entry point
+ * print_alphabet_x10 - prints a to z 10 times
  *
- * Description - 'Task1 print alphabet lowercase'
- * @void : the function takes no parameteres
-*/
+ * Description: 'task 2'
+ *
+ * @void: does not take any parameters
+ *
+ * Return: Always 0 (Success)
+ */
 
 void print_alphabet_x10(void)
 {
