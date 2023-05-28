4303bd2d1ca48580fc71d11d6e014c86b473b2ff
MXCALL <idbellam12@gmail.Com>
2023-05-28 13:40:26 +0100

    TASK5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @void: The FunctionTakes No Parameters
+ *
+ * Description: 'TASK5 Print 0 to 10 base 10 using putchar'
+ *
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+
+int A;
+for (A = 48; A < 58; A++)
+putchar(A);
+putchar('\n');
+
+
+return (0);
+}
