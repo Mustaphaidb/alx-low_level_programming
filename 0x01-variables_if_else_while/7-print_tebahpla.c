97d76856f9ec5b7fa830231170bb333920f13490
MXCALL <idbellam12@gmail.Com>
2023-05-28 14:25:48 +0100

    TASK7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+
+char reverse;
+
+for (reverse = 122 ; reverse >= 97; reverse--)
+{
+    putchar(reverse);
+}
+
+    putchar('\n');
+
+
+
+
+
+return (0);
+}
