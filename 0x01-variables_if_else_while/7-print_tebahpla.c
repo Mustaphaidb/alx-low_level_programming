23f7b2a905a913b67a090f2899241d066371bcd1
MXCALL <idbellam12@gmail.Com>
2023-05-28 14:29:48 +0100

    TASK7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - Entry Point
+ *
+ * @void: The Function Takes no Parametres
+ *
+ * Description: 'TASK7 Print Alphabets in reverse using putchar'
+ *
+ *
+ * Return: 0 (Success)
+ *
+*/
 int main(void)
 {
 
@@ -7,15 +17,8 @@ int main(void)
 char reverse;
 
 for (reverse = 122 ; reverse >= 97; reverse--)
-{
-    putchar(reverse);
-}
+putchar(reverse);
 
-    putchar('\n');
-
-
-
-
-
+putchar('\n');
 return (0);
 }
