b8495bcb9715b15dee05ba3f3317c68edd54ee67
MXCALL <idbellam12@gmail.Com>
2023-05-31 03:40:48 +0100

    TASK2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,19 @@
 #include "main.h"
-
+/**
+ * _strlen - int function
+ * @s: char pointer
+ * Description: _strlen
+ * Return: int
+ */
 
 int _strlen(char *s)
 {
 
 int len;
 
-for (len = 0;*s != '\0' ; s++)
+for (len = 0; *s != '\0' ; s++)
 ++len;
 
-return(len);
+return (len);
 
 }
