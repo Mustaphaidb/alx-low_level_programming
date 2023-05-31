1c5293e9be4083c0b78911950f181b97437c7d46
MXCALL <idbellam12@gmail.Com>
2023-05-31 02:34:29 +0100

    TASK0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reset_to_98 - Entry Point
+ *
+ * @n : int pointer
+ *
+ * Description: change the value of n to 98
+ *
+ * Return: Void
+*/
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
